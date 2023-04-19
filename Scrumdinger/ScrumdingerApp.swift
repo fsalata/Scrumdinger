1fe7aba1e33c9fb5aaab5417e7a8391c86016269
Fábio Salata <fsalata@gmail.com>
2023-04-18 21:11:25 -0300

    Adding project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Fabio Salata on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
