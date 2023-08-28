bcc38886f87d184531af450adc9fa82dd14e588b
Fábio Salata <fsalata@gmail.com>
2023-08-28 14:57:13 -0300

    Detail view and navigation

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,58 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Fabio Salata on 28/08/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                    .foregroundColor(.accentColor)
+                }
+
+                HStack {
+                    Label("Legth", systemImage: "clock")
+
+                    Spacer()
+
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+
+                    Spacer()
+
+                    Text("\(scrum.theme.name)")
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+            }
+
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }
+        .navigationTitle(scrum.title)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
