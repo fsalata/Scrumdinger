b4b3942c03ad066d96b09c21235f233651b8b739
Fábio Salata <fsalata@gmail.com>
2023-09-01 15:42:14 -0300

    Theme selector

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,25 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Fabio Salata on 01/09/23.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
